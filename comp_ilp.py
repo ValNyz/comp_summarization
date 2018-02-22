521b836be9651fef436031e463e0e96998319f4b
valnyz <valentin.nyzam@gmail.com>
2018-02-22 17:32:30 +0100

    Move comp_ilp / comp_we into class

--- comp_ilp.py
+++ comp_ilp.py
@@ -18,11 +18,9 @@ logger = logging.getLogger(__name__)
 
 
 def score_sentence_ilp(*l_sents):
-
-    c_ij, s_ik, l_ik, tf_i = make_concept(*l_sents)
-    print("Vocabulary size : " + str(len(c_ij[0]) + len(c_ij[1])))
-    u_jk = make_concept_pair(c_ij, tf_i, relevance_wordnet)
-    generate_ilp_problem(c_ij, tf_i, u_jk, s_ik, l_ik)
+    ilp = ilp_wordnet(l_sents)
+    ilp.prepare()
+    generate_ilp_problem(ilp.c_ij, ilp.tf_i, ilp.u_jk, ilp.s_ik, ilp.l_ik)
     return None
 
 
@@ -35,14 +33,14 @@ def generate_ilp_problem(c_ij, w_ij, u_jk, s_ik, l_ik):
     :param s_ik: list[list[list[concept]]]: list doc as a list of sentence as a
     list of concept
     """
-    prob = pulp.LpProblem('summarize', pulp.LpMaximize)
+    prob = pulp.lpproblem('summarize', pulp.lpmaximize)
 
     # oc_ij
-    oc_ij = pulp.LpVariable.dicts('concepts', [(i, j) for i in range(len(c_ij))
+    oc_ij = pulp.lpvariable.dicts('concepts', [(i, j) for i in range(len(c_ij))
                                                for j in range(len(c_ij[i]))],
-                                  0, 1, pulp.LpBinary)
+                                  0, 1, pulp.lpbinary)
     # op_jk
-    op_jk = pulp.LpVariable.dicts('pairs', [(j, k) for j in range(len(u_jk))
+    op_jk = pulp.lpvariable.dicts('pairs', [(j, k) for j in range(len(u_jk))
                                             for k in range(len(u_jk[j]))],
                                   0, 1, pulp.LpBinary)
     # ocs_ijk
@@ -88,61 +86,129 @@ def generate_ilp_problem(c_ij, w_ij, u_jk, s_ik, l_ik):
     return None
 
 
-def make_concept(*l_sents, order=2):
-    """make_concept
+class ilp(object):
+    def __init__(self, *l_sents):
+        self.l_sents = l_sents
+        self.c_ij = []
+        self.s_ik = []
+        self.l_ik = []
+        self.w_ij = []
 
-    :param *l_sents:
-    :param order:
-    :return list[list[concept]]: list of all concepts per doc
-    :return list[list[list[concept]]]: list of doc as list of sentence as list
-    of concepts
-    :return list[dict{concept:tf}]
-    """
-    c_ij = []
-    s_ik = []
-    l_ik = []
-    tf_i = []
-    for i, doc in enumerate(l_sents):
-        c_ij.append([])
-        s_ik.append(set())
-        l_ik.append([])
-        for sentence in doc:
-            sent = sentence.get_list_word_pos()
-            temp = list(ngrams(sent, order))
-            temp.extend(sent)
-            s_ik[i].update(temp)
-            l_ik[i].append(len(sent))
-            c_ij[i].extend(temp)
-        tf_i.append(Counter(c_ij[i]))
-        c_ij[i] = set(c_ij[i])
-    return c_ij, s_ik, l_ik, tf_i
+    def prepare(self):
+        pass
 
 
-def make_concept_pair(c_ij, w_ij, similarity, threshold=0.2):
-    """make_dict_concept_pair
+class ilp_wordnet(object):
+    def prepare(self):
+        self._make_concept()
+        print("Vocab size : " + str(len(self.c_ij[0]) + len(self.c_ij[1])))
+        threshold = 0.2
+        if os.path.exists():
+            self.u_jk = read_concept_pair('pair_ilp_wordnet_' + str(threshold))
+        else:
+            self.u_jk = self._make_concept_pair(self.c_ij, self.w_ij)
 
-    :param c_ij:
-    :param w_ij:
-    :param similarity: similarity function: input(concept, concept):
-        ouput(0<int<1)
-    :param threshold:
-    :return list[np.array]: size [j, k]
-    """
-    u_jk = []
-    # build list pair concept
-    for j, c_0 in enumerate(c_ij[0]):
-        u_jk.append(np.zeros(len(c_ij[1]), float))
-        for k, c_1 in enumerate(c_ij[1]):
-            if similarity(c_0, c_1) > threshold:
-                u_jk[j][k] = (w_ij[0][c_0]+w_ij[1][c_1])/2
-            else:
-                u_jk[j][k] = 0
-    with open('pair_' + str(similarity) + '_' + str(threshold), 'w') as f:
-        for j in range(len(u_jk)):
-            for k in range(len(u_jk[j])):
-                f.write(str(c_ij[0][j]) + '\t' + str(c_ij[1][k]) + '\t' +
-                        str(u_jk[j][k]))
-    return u_jk
+    def _make_concept(self, order=2):
+        """make_concept
+        :param *l_sents:
+        :param order:
+        :return list[list[concept]]: list of all concepts per doc
+        :return list[list[list[concept]]]: list of doc as list of sentence as
+        list
+        of concepts
+        :return list[dict{concept:tf}]
+        """
+        for i, doc in enumerate(self.l_sents):
+            self.c_ij.append([])
+            self.s_ik.append(set())
+            self.l_ik.append([])
+            for sentence in doc:
+                sent = sentence.get_list_word_pos()
+                temp = list(ngrams(sent, order))
+                temp.extend(sent)
+                self.s_ik[i].update(temp)
+                self.l_ik[i].append(len(sent))
+                self.c_ij[i].extend(temp)
+            self.w_ij.append(Counter(self.c_ij[i]))
+            self.c_ij[i] = set(self.c_ij[i])
+        return self.c_ij, self.s_ik, self.l_ik, self.w_ij
+
+    def _make_concept_pair(self, threshold):
+        """make_dict_concept_pair
+
+        :param c_ij:
+        :param w_ij:
+        :param similarity: similarity function: input(concept, concept):
+            ouput(0<int<1)
+        :param threshold:
+        :return list[np.array]: size [j, k]
+        """
+        u_jk = []
+        # build list pair concept
+        for j, c_0 in enumerate(self.c_ij[0]):
+            u_jk.append(np.zeros(len(self.c_ij[1]), float))
+            for k, c_1 in enumerate(self.c_ij[1]):
+                if self._relevance_wordnet(c_0, c_1) > threshold:
+                    u_jk[j][k] = (self.w_ij[0][c_0]+self.w_ij[1][c_1])/2
+                else:
+                    u_jk[j][k] = 0
+        with open('pair_ilp_wordnet_' + str(threshold),
+                  'w') as f:
+            for j in range(len(u_jk)):
+                for k in range(len(u_jk[j])):
+                    f.write(str(self.c_ij[0][j]) + '\t' + str(self.c_ij[1][k])
+                            + '\t' + str(u_jk[j][k]))
+        return u_jk
+
+
+    def _relevance_wordnet(cls, c_0, c_1):
+        # print(str(c_0) + "\t" + str(c_1))
+        if type(c_0[0]) == type(()):
+            allsyns1 = set(ss for word in c_0 for ss in
+                           wn.synsets(word[0], pos=transform_POS(word[1])))
+        else:
+            allsyns1 = set(ss for ss in wn.synsets(c_0[0],
+                                                   pos=transform_POS(c_0[1])))
+        if type(c_1[0]) == type(()):
+            allsyns2 = set(ss for word in c_1 for ss in
+                           wn.synsets(word[0], pos=transform_POS(word[1]))
+                           if ss not in allsyns1)
+        else:
+            allsyns2 = set(ss for ss in wn.synsets(c_1[0],
+                                                   pos=transform_POS(c_1[1])))
+
+        if len(allsyns1) == 0 or len(allsyns2) == 0:
+            return 0
+        list_sim = [wn.wup_similarity(s1, s2) or 0 for s1, s2 in
+                    product(allsyns1, allsyns2)]
+        best = sum(list_sim)/len(list_sim)
+        # print(best)
+        return best
+
+
+def ilp_we(object):
+    def __init__(self, model_we, *l_sents):
+        ilp.__init_(l_sents)
+        self.model = model_we
+
+    def prepare(self):
+        self._update_model()
+        self._make_concept()
+        print("Vocab size : " + str(len(self.c_ij[0]) + len(self.c_ij[1])))
+        threshold = 0.2
+        if os.path.exists():
+            self.u_jk = read_concept_pair('pair_ilp_wordembdeddings_' + str(threshold))
+        else:
+            self.u_jk = self._make_concept_pair(self.c_ij, self.w_ij)
+
+    def _update_model(self):
+        """_update_model
+        update word embeddings model on self.l_sents for unseen word
+        """
+
+    def _make_concept(self):
+
+    def _make_concept_pair(self):
 
 
 def read_concept_pair(file_name, c_ij):
@@ -159,31 +225,6 @@ def read_concept_pair(file_name, c_ij):
                 u_jk.append(np.zeros(len(c_ij[1]), float))
             k += 1
         u_jk[j][k] = pair[2]
-
-
-def relevance_wordnet(c_0, c_1):
-    # print(str(c_0) + "\t" + str(c_1))
-    if type(c_0[0]) == type(()):
-        allsyns1 = set(ss for word in c_0 for ss in
-                       wn.synsets(word[0], pos=transform_POS(word[1])))
-    else:
-        allsyns1 = set(ss for ss in wn.synsets(c_0[0],
-                                               pos=transform_POS(c_0[1])))
-    if type(c_1[0]) == type(()):
-        allsyns2 = set(ss for word in c_1 for ss in
-                       wn.synsets(word[0], pos=transform_POS(word[1]))
-                       if ss not in allsyns1)
-    else:
-        allsyns2 = set(ss for ss in wn.synsets(c_1[0],
-                                               pos=transform_POS(c_1[1])))
-
-    if len(allsyns1) == 0 or len(allsyns2) == 0:
-        return 0
-    list_sim = [wn.wup_similarity(s1, s2) or 0 for s1, s2 in
-                product(allsyns1, allsyns2)]
-    best = sum(list_sim)/len(list_sim)
-    # print(best)
-    return best
 
 
 def cosine_similarity(c_0, c_1):
