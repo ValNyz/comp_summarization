36cd205cf1b4ba305e40f3a1d2ad8fc8adbc146b
valnyz <valentin.nyzam@gmail.com>
2018-02-23 10:40:14 +0100

    Add word embeddings

--- comp_ilp.py
+++ comp_ilp.py
@@ -5,12 +5,14 @@ Comparative summarization method using ILP
 Comparative News Summarization Using Linear Programming (Huang et al, 2011)
 __author__ : Valentin Nyzam
 """
-
-from itertools import product
+import os
+import gensim.models
+from itertools import product, chain
 from collections import Counter
 from nltk.util import ngrams
 from nltk.corpus import wordnet as wn
 import numpy as np
+from scipy.spatial.distance import cosine
 import pulp
 from pulp import lpSum
 import logging
@@ -53,9 +55,9 @@ def generate_ilp_problem(c_ij, w_ij, u_jk, s_ik, l_ik):
                                   0, 1, pulp.LpBinary)
     lambd = 0.55
     prob += lambd*lpSum([u_jk[j][k]*op_jk[(j, k)] for j in range(len(c_ij[0]))
-                         for k in range(len(c_ij[1]))]) + \
-            (1-lambd)*lpSum([w_ij[i][c_ij[i][j]]*oc_ij[(i, j)] for i in
-                             range(len(c_ij)) for j in range(len(c_ij[i]))])
+                         for k in range(len(c_ij[1]))]) + (1-lambd)*lpSum(
+                             [w_ij[i][c_ij[i][j]]*oc_ij[(i, j)] for i in
+                              range(len(c_ij)) for j in range(len(c_ij[i]))])
     # prob += pulp.lpSum([tf[w] * word_vars[w] for w in tf])
     for j in range(len(c_ij[0])):
         for k in range(len(c_ij[1])):
@@ -95,27 +97,16 @@ class ilp(object):
         self.w_ij = []
 
     def prepare(self):
-        pass
-
-
-class ilp_wordnet(object):
-    def prepare(self):
         self._make_concept()
         print("Vocab size : " + str(len(self.c_ij[0]) + len(self.c_ij[1])))
-        threshold = 0.2
-        if os.path.exists():
-            self.u_jk = read_concept_pair('pair_ilp_wordnet_' + str(threshold))
-        else:
-            self.u_jk = self._make_concept_pair(self.c_ij, self.w_ij)
 
-    def _make_concept(self, order=2):
-        """make_concept
+    def _make_concept(self):
+        """_make_concept
         :param *l_sents:
         :param order:
         :return list[list[concept]]: list of all concepts per doc
         :return list[list[list[concept]]]: list of doc as list of sentence as
-        list
-        of concepts
+        list of concepts
         :return list[dict{concept:tf}]
         """
         for i, doc in enumerate(self.l_sents):
@@ -133,9 +124,18 @@ class ilp_wordnet(object):
             self.c_ij[i] = set(self.c_ij[i])
         return self.c_ij, self.s_ik, self.l_ik, self.w_ij
 
+
+class ilp_wordnet(ilp):
+    def prepare(self):
+        ilp.prepare()
+        threshold = 0.2
+        if os.path.exists():
+            self.u_jk = read_concept_pair('pair_ilp_wordnet_' + str(threshold))
+        else:
+            self.u_jk = self._make_concept_pair(self.c_ij, self.w_ij)
+
     def _make_concept_pair(self, threshold):
         """make_dict_concept_pair
-
         :param c_ij:
         :param w_ij:
         :param similarity: similarity function: input(concept, concept):
@@ -159,7 +159,6 @@ class ilp_wordnet(object):
                     f.write(str(self.c_ij[0][j]) + '\t' + str(self.c_ij[1][k])
                             + '\t' + str(u_jk[j][k]))
         return u_jk
-
 
     def _relevance_wordnet(cls, c_0, c_1):
         # print(str(c_0) + "\t" + str(c_1))
@@ -186,10 +185,10 @@ class ilp_wordnet(object):
         return best
 
 
-def ilp_we(object):
-    def __init__(self, model_we, *l_sents):
+def ilp_we(ilp):
+    def __init__(self, model_name, *l_sents):
         ilp.__init_(l_sents)
-        self.model = model_we
+        self._update_model(model_name)
 
     def prepare(self):
         self._update_model()
@@ -197,18 +196,58 @@ def ilp_we(object):
         print("Vocab size : " + str(len(self.c_ij[0]) + len(self.c_ij[1])))
         threshold = 0.2
         if os.path.exists():
-            self.u_jk = read_concept_pair('pair_ilp_wordembdeddings_' + str(threshold))
+            self.u_jk = read_concept_pair('pair_ilp_wordembdeddings_' +
+                                          str(threshold))
         else:
             self.u_jk = self._make_concept_pair(self.c_ij, self.w_ij)
 
-    def _update_model(self):
+    def _update_model(self, model_name):
         """_update_model
         update word embeddings model on self.l_sents for unseen word
         """
+        self.model = gensim.models.Word2Vec.load(model_name)
+        self.model.train(chain(self.l_sents))
+        return model
 
-    def _make_concept(self):
 
     def _make_concept_pair(self):
+        """make_dict_concept_pair
+        :param c_ij:
+        :param w_ij:
+        :param similarity: similarity function: input(concept, concept):
+            ouput(0<int<1)
+        :param threshold:
+        :return list[np.array]: size [j, k]
+        """
+        u_jk = []
+        # build list pair concept
+        for j, c_0 in enumerate(self.c_ij[0]):
+            u_jk.append(np.zeros(len(self.c_ij[1]), float))
+            for k, c_1 in enumerate(self.c_ij[1]):
+                sim = self._cosine_similarity(c_0, c_1)
+                if sim > threshold:
+                    u_jk[j][k] = sim
+                    # (self.w_ij[0][c_0]+self.w_ij[1][c_1])/2
+                else:
+                    u_jk[j][k] = 0
+        with open('pair_ilp_we_' + str(threshold), 'w') as f:
+            for j in range(len(u_jk)):
+                for k in range(len(u_jk[j])):
+                    f.write(str(self.c_ij[0][j]) + '\t' + str(self.c_ij[1][k])
+                            + '\t' + str(u_jk[j][k]))
+        return u_jk
+
+
+    def _cosine_similarity(self, c_0, c_1):
+        if type(c_0[0]) != type(()):
+            c_0 = (c_0)
+        if type(c_1[0]) != type(()):
+            c_1 = (c_1)
+
+        list_sim = [cosine_similarity(self.model, w1, w2) for w1 in t1 for t1 in c_0 for w2
+                   in t2 for t2 in c_1]
+        best = sum(list_sim)/len(list_sim)
+        return best
 
 
 def read_concept_pair(file_name, c_ij):
@@ -227,8 +266,8 @@ def read_concept_pair(file_name, c_ij):
         u_jk[j][k] = pair[2]
 
 
-def cosine_similarity(c_0, c_1):
-    return 0
+def cosine_similarity(model, w1, w2):
+    return 1-cosine(model[w1], model[w2])
 
 
 WN_NOUN = 'n'
