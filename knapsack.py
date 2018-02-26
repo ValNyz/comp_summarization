a55ce26adb14b5854476f2f7f2fd300f26bb6007
valnyz <valentin.nyzam@gmail.com>
2018-02-26 15:20:51 +0100

    Knapsack update

--- knapsack.py
+++ knapsack.py
@@ -7,40 +7,27 @@ Inspired by :
     Comparative News Summarization Using Linear Programming (Huang et al, 2011)
 __author__ : Valentin Nyzam
 """
-import os
-import gensim.models
-from itertools import product, chain
-from collections import Counter
-from nltk.util import ngrams
-from nltk.corpus import wordnet as wn
-import numpy as np
-from scipy.spatial.distance import cosine
-import pulp
-from pulp import lpSum
+import ilp
 import logging
 logger = logging.getLogger(__name__)
 
 
 def score_sentence_knapsack(*l_sents):
+    kp = ilp.Ilp_we('testWiki', l_sents)
+    kp.prepare()
+    ocs_ikj = [[[1 if j in k else 0 for j in kp.c_ij[i]] for k in kp.s_ik[i]]
+               for i in range(len(kp.c_ij))]
+    summary = knapsack(200, ocs_ikj, kp.w_ij, kp.u_jk, kp.s_ik, kp.l_ik)
+
+    for sent in summary:
+        print(l_sents[sent[0]][sent[1]])
+
     return None
-
-
-def generate_knapsack_problem(c_ij, w_ij, u_jk, s_ik, l_ik):
-    """generate_ilp_problem
-    :param c_ij: list[list[concept]]: list of concept: i #doc, j #concept
-    :param w_ij: list[dict{concept:weight}]: dict of concept:weight for each
-    document
-    :param u_jk: dict{concept_pair:weight}: dict of concept_pair:weight
-    :param s_ik: list[list[list[concept]]]: list doc as a list of sentence as a
-    list of concept
-    """
-    ocs_ikj = [[[0 if j in k else 1 for j in c_ij[i]] for k in s_ik[i]] for
-               i in range(len(c_ij))]
 
 
 # A Dynamic Programming based Python Program for 0-1 Knapsack problem
 # Returns the maximum value that can be put in a knapsack of capacity W
-def knapsack(sumSize, c_ij, w_ij, u_jk, s_ik, l_ik):
+def knapsack(sumSize, ocs_ikj, w_ij, u_jk, s_ik, l_ik):
     """knapsack
 
     :param sumSize:
@@ -52,32 +39,54 @@ def knapsack(sumSize, c_ij, w_ij, u_jk, s_ik, l_ik):
     """
     # K = cube of (value, summary (as a list of tuple (as coordinate of
     # sentence)))
-    K = [[[(0, []) for w in range(sumSize+1)]
+    K = [[[[0, []] for w in range(sumSize + 1)]
           for s1 in range(len(s_ik[1]) + 1)]
          for s0 in range(len(s_ik[0]) + 1)]
 
     # Build table K[][] in bottom up manner
-    for s0 in range(len(s_ik[0])):
-        for s1 in range(len(s_ik[1])):
-            for w in range(sumSize+1):
+    for s0 in range(len(s_ik[0]) + 1):
+        for s1 in range(len(s_ik[1]) + 1):
+            for w in range(sumSize + 1):
                 if s0 == 0 or s1 == 0 or w == 0:
-                    K[s0][s1][w] = (0, [])
-                elif l_ik[0][s0]+l_ik[1][s1-1] <= w and l_ik[1][s1] <= w:
-                    value = obj(K[s0][s1][w][1])
-                    if value > K[s0][s1][w][0]:
-                        val[i-1] + K[i-1][w-wt[i-1]]
-                        K[s0][s1][w][1].extend(K[s0][s1-1][w][1])
-                        K[s0][s1][w][1].append((0, s0))
-                        K[s0][s1][w][1].append((1, s1))
+                    # K[s0][s1][w] = (0, [])
+                    pass
+                elif l_ik[0][s0-1] + l_ik[1][s1-1] <= w:
+                    current_sum = list(K[s0][s1-1]
+                                        [w-l_ik[0][s0-1]-l_ik[1][s1-1]]
+                                        [1])
+                    current_sum.append((0, s0-1))
+                    current_sum.append((1, s1-1))
+                    value = obj(0.55, ocs_ikj, w_ij, u_jk, current_sum)
+                    if value > K[s0][s1-1][w][0]:
                         K[s0][s1][w][0] = value
+                        K[s0][s1][w][1] = current_sum
                     else:
                         K[s0][s1][w] = K[s0][s1-1][w]
-                    K[s0][s1][w] = max(objective_func[i-1] + K[i-1][w-wt[i-1]],  K[i-1][w])
                 else:
-                    K[i][w] = K[i-1][w]
+                    K[s0][s1][w] = K[s0][s1-1][w]
+    print(K)
+    return K[len(s_ik[0])+1][len(s_ik[1]+1)][sumSize+1]
 
-    return K[n][sumSize]
 
-
-def obj(summary):
-    pass
+def obj(lambd, ocs_ikj, w_ij, u_jk, summary):
+    comp = 0.
+    rep = 0.
+    lc_0 =set()
+    lc_1 = set()
+    for sen in summary:
+        # sen = (i, k)
+        if sen[0] == 0:
+            lc_0.update([j for j in range(len(ocs_ikj[0][sen[1]]))])
+        elif sen[0] == 1:
+            lc_1.update([j for j in range(len(ocs_ikj[1][sen[1]]))])
+        else:
+            pass
+    t_rep = False
+    for c_0 in lc_0:
+        rep += w_ij[0][c_0]
+        for c_1 in lc_1:
+            if not t_rep:
+                rep += w_ij[1][c_1]
+            comp += u_jk[c_0][c_1]
+        t_rep = True
+    return lambd*comp + (1-lambd)*rep
